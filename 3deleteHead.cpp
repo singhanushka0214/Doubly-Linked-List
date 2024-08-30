fdbcb0a943caae092ad102e670cb27512be721db
Anushka Singh <97422676+singhanushka0214@users.noreply.github.com>
2024-08-30 17:03:19 +0530

    Add files via upload

---
+++ 3deleteHead.cpp
@@ -0,0 +1,58 @@
+// Creation Of Doubly Linked List
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1, Node* next1, Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+void print(Node* head){
+    while(head){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+Node* convertArr2DLL(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+Node* deeleteHead(Node* head){
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* prev = head;
+    head = head->next;
+
+    head->back = nullptr;
+    prev->next = nullptr;
+
+    delete prev;
+    return head;
+}
+int main(){
+    vector<int> arr = {1,2,3,4};
+    Node* head = convertArr2DLL(arr);
+    head = deeleteHead(head);
+    print(head);
+    return 0;
+}
